7df9dba1c2fb60693e41d98704cf25fc51c1a6c1
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2021-04-13 16:05:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 for(i=1; i<=100; i++){
-            document.write(i%3 === 0 ? (i%5 === 0 ? 'ANSWER ' : 'Wishy ') : (i%5 === 0 ? 'WASHY ' : i+' '));
+            document.write(i%3 === 0 ? (i%5 === 0 ? 'ANSWER ' : 'WISHY ') : (i%5 === 0 ? 'WASHY ' : i+' '));
         };
